cf081d8b281c4dd023c7eefa827a7f0a3a849df0
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-07-31 13:30:16 +0300

    Hello world

---
+++ bin/first_fork.dart
@@ -0,0 +1,5 @@
+import 'package:first_fork/first_fork.dart' as first_fork;
+
+void main(List<String> arguments) {
+  print('Hello world: ${first_fork.calculate()}!');
+}
---
+++ test/first_fork_test.dart
@@ -0,0 +1,8 @@
+import 'package:first_fork/first_fork.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
